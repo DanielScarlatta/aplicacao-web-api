0daea6df40f27669ed8e9b5df726652127ccd1f3
Daniel Dias Scarlatta <daniel.scarlatta@capitalconsig.local>
2023-11-24 09:13:24 -0300

    chamando a api no register

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,15 +1,25 @@
-const url = "http://54.94.34.44:4000"
+const url = "http://18.231.124.162:4000"
 
+const nameRegister = document.getElementById('nameRegister')
 const emailRegister = document.getElementById('emailRegister')
 const passwordRegister = document.getElementById('passwordRegister')
-const confirmPasswordRegister = document.getElementById('confirmPasswordRegister')
+const confirmPasswordRegister = document.getElementById('confirmpasswordRegister')
 const btnRegisterUser = document.getElementById('btnRegisterUser')
 const conteinerForm = document.getElementById('conteinerForm')
 
 
 conteinerForm.addEventListener('submit', (ev) => {
   ev.preventDefault()
-  hello()
+  let dataUser = {
+    name: nameRegister.value,
+    email: emailRegister.value,
+    password: passwordRegister.value,
+    confirmpassword: confirmPasswordRegister.value,
+  }
+
+  dataUser = JSON.stringify(dataUser);
+
+  registerUser(dataUser)
 })
 
 
@@ -23,8 +33,14 @@ async function hello() {
   console.log(data)
 }
 
-async function registerUser() {
-  const response = await fetch(url + "/v1/auth/register", );
+async function registerUser(dataUser) {
+  const response = await fetch(`${url}/v1/auth/register`, {
+    method: "POST",
+    body: dataUser,
+    headers: {
+      "Content-Type": "application/json",
+    }
+  });
 
   console.log(response)
 
